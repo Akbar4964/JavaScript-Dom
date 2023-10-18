84e6a7c5a2c8b595a4479cfd2b66499d45e4c14c
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-10-18 10:45:15 +0500

    added codes

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+// document.addEventListener("click", () => {
+//   document.body.classList.toggle("Salom");
+// });
+const elBtns = document.querySelector(".btns");
+const elBtns1 = document.querySelector(".btns1");
+const click = document.getElementsByClassName("clicker");
+click[0].addEventListener("click", (evt) => {
+  console.log("clickers");
+  document.body.classList.toggle("salom");
+  evt.stopPropagation();
+});
+
+elBtns.addEventListener("click", btnsClick);
+elBtns1.addEventListener("click", btnsClick1);
+
+function btnsClick(evt) {
+  console.log("btnds");
+}
+
+function btnsClick1(evt) {
+  console.log("btns1");
+
+  evt.stopPropagation();
+}
+// function btnsClick1(evt) {
+//   console.log("btns1");
+
+//   evt.stopPropagation();
+// }function btnsClick1(evt) {
+//   console.log("btns1");
+
+//   evt.stopPropagation();
+// }
